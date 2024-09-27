9f09d1cf04d7dbd6422c3de193307976f1290441
Alessandro <alessandro.s401381@cesurformacion.com>
2024-09-27 13:13:20 +0200

    primer commit

---
+++ Programacion/Suma.java
@@ -0,0 +1,10 @@
+package Programacion;
+
+public class Suma {
+
+    public static void main(String[] args) {
+        int suma = 1+2+3+4+5;
+        System.out.println(suma);;
+        
+    }
+}
---
+++ Programacion/suma.py
@@ -0,0 +1,11 @@
+def sumaNumeros():
+    suma = float(1+2+3+4+5)
+    print(suma)
+
+print("Se puede definir una función con print() dentro")
+sumaNumeros()
+
+suma2 = int(2+3+4+5+6)
+print(suma2)
+
+print("O se puede hacer sin definir una función")
