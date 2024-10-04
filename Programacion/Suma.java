4d44579a44cbba9d3f5f740cd9e414a7422fd78f
Alessandro <alessandro.s401381@cesurformacion.com>
2024-10-04 10:34:41 +0200

    cambio en suma.java

--- Programacion/Suma.java
+++ Programacion/Suma.java
@@ -1,10 +1,9 @@
 package Programacion;
 
 public class Suma {
-
     public static void main(String[] args) {
         int suma = 1+2+3+4+5;
-        System.out.println(suma);;
+        System.out.println("La suma de 1 + 2 + 3 + 4 + 5 = " + suma);;
         
     }
 }
