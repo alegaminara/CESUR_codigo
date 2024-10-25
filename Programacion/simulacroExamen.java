c77b5002ef56029c83b4d7133a9eec496fe3b580
Alessandro Gaminara <alessandro.s401381@cesurformacion.com>
2024-10-25 08:58:40 +0200

    simulacro de examen 25-10

---
+++ Programacion/simulacroExamen.java
@@ -0,0 +1,23 @@
+public class simulacroExamen {
+    public static int numero(int A, int B){
+        if (A >= B) {
+            return A;
+        } else {
+            return B;
+        }
+    }
+    public static void main(String[] args) {
+        numero(5, 6);
+    }
+}
+
+// Mi código
+//
+// public class main {
+//  public class int numeroMax (int A, int B) {
+//      return Math.max(numeroMax);
+//  }
+//  public static void main (String[] args) {
+//      numeroMax(A,B);
+//  }  
+// }
